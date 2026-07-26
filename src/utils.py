b83c4c24a37d2969992036c4ff01db5551bc5a21
Noman <abdullahccse.cou14@gmail.com>
2026-07-27 01:10:28 +0600

    Model traning done

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from sklearn.metrics import r2_score
 from src.exception import CustomException
 from src.logger import logging
 import sys
@@ -7,9 +8,29 @@ import sys
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-        
+
         os.makedirs(dir_path, exist_ok=True)
         pd.to_pickle(obj, file_path)
         logging.info(f"Object saved to {file_path}")
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            # Train model
+            model.fit(X_train, y_train)
+
+            # Predict test data
+            y_test_pred = model.predict(X_test)
+
+            # Evaluate model
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
     except Exception as e:
         raise CustomException(e, sys)
