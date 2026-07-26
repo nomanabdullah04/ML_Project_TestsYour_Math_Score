2b8ec9686ee7c81681298e78cba422a67afe1b7e
Noman <abdullahccse.cou14@gmail.com>
2026-07-26 16:23:07 +0600

    The EDA AND Modeling

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -14,3 +14,10 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+
+try:
+    # Example code that may raise an exception
+    x = 1 / 0
+except Exception as e:
+    logging.error("Division by zero error occurred.")
+    raise CustomException(e, sys)
