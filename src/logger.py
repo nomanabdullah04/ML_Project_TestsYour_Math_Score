af9e64a3424f9b0680b46d16b5ec715c68f5bc3d
Noman <abdullahccse.cou14@gmail.com>
2026-07-26 13:09:15 +0600

    logging and exception

--- src/logger.py
+++ src/logger.py
@@ -17,7 +17,3 @@ logging.basicConfig(
     format="[%(asctime)s] %(levelname)s - %(message)s",
     level=logging.INFO
 )
-
-if __name__ == "__main__":
-    logging.info("Logging has started.")
-    print("Log file generated successfully!")
